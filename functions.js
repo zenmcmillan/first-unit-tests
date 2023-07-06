5de8ed77bad688d31ed21c63ad840d1d4671e1f4
Zen McMillan <zenmcmillan@gmail.com>
2023-07-05 22:21:48 -0600

    Create functions to pass all unit tests

--- functions.js
+++ functions.js
@@ -1,9 +1,32 @@
 // functions.js
-function addTwoNumbers() {
+
+function addTwoNumbers(num1, num2) {
+  return num1 + num2
 }
+
+function sayHello(theName) {
+  if (theName === 'Will') {
+    return 'No more testing Will!'
+  } else {
+    return "Hi there " + theName + "!"
+  }
+}
+
+function buildCar(theColor, vehicle) {
+  if (vehicle) {
+    return carInfo = {type: 'truck'}
+  } else if (!vehicle && theColor) {
+    return carDetails = {color: theColor}
+  }
+  else return {}
+  }  
+
+
+
+
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+     sayHello,
+     buildCar
 }
